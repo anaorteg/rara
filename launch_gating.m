668dfc3f3644e52e03c17d41f2f765261fc4fbac
nikhilmurty <nikhilsanjaymurty@gmail.com>
2017-06-14 17:00:24 +0200

    Fixed the masking

--- launch_gating.m
+++ launch_gating.m
@@ -4,7 +4,7 @@ close all
 clc
 
 %Initialize DIP
-dip_initialise();
+% dip_initialise();
 
 %Add folders to the path
 here = mfilename('fullpath');
@@ -13,7 +13,7 @@ addpath(genpath(path));
 
 %% *********************** ACQ Parameters *********************************
 % Data paths and files
-acqPath = 'D:\nmurty\data\8\1445266740835\'; %KEEP THE "\"
+acqPath = 'D:\nmurty\data\73\1453808242879\'; %KEEP THE "\"
 binning= 4; %Binning of acquisition
 nos = 514; %number of steps
 n_angles_step = 8; %number of projections per step
@@ -28,17 +28,17 @@ height = 1944/binning; % 486
 % With '\' at the end
 procPath = [acqPath 'preproc\'];
 
-[data_table, path_dest, brate] = respSignal(procPath,width,height,nos,n_angles_step,fmt);
+%[data_table, path_dest, brate] = respSignal(procPath,width,height,nos,n_angles_step,fmt);
 
 %  path_dest = [procPath 'p1\subvolume00\'];
 %  load([procPath 'p1\data_table.mat'], 'data_table')
- [data_table, path_dest, hrate] = cardSignal(path_dest,width,height,nos,n_angles_step,fmt,data_table, procPath);
+% [data_table, path_dest, hrate] = cardSignal(path_dest,width,height,nos,n_angles_step,fmt,data_table, procPath);
 
-% path_dest = [procPath 'c1\subvolume00\'];
-% load([procPath 'c1\data_table.mat'], 'data_table')
+ path_dest = [procPath 'c1\subvolume00\'];
+ load([procPath 'c1\data_table.mat'], 'data_table')
 % path_dest=procPath; %uncomment if want to calibrate all the files
 % data_table(3,:) = 1; %uncomment if want to calibrate all the files
 avgStep(data_table, binning, nos, n_angles_step, width, height, procPath, path_dest, fmt);
 
 %display breathing and heart rates
-fprintf('The average breathing rate is %.2f beats per minute. \n The average heart rate is %.2f beats per minute. \n',brate,hrate)
+%fprintf('The average breathing rate is %.2f beats per minute.\n The average heart rate is %.2f beats per minute.\n',brate,hrate)
--- maskIm.m
+++ maskIm.m
@@ -1,4 +1,4 @@
-function [ intenVol ] = maskIm( intenROI, intenVol,file,frame,px_x,px_y,nop_f,fmt,ct, acqPath )
+function [ intenVol_masked ] = maskIm( intenROI, intenVol,file,frame,px_x,px_y,nop_f,fmt,ct, acqPath )
 %Finding the ROI and masking the image
 %   Get user to input initial data to find the ROI and then run the masking
 %   program to block unwanted parts of intenVol for all the images
@@ -13,13 +13,25 @@ function [ intenVol ] = maskIm( intenROI, intenVol,file,frame,px_x,px_y,nop_f,fm
         
     %% Masking the projection
     %see fundamentals for help on this P. 48, 104
-    dip_initialise;
-    [hist, bins]= diphist(intenVol(:,:,1),[]);
-    [pks,loc] =findpeaks(hist);
     
-    %use size of peaks to find last and second to last and then get the
-    %threshold
+    %get the bin number for the threshold
+    [hist, bins]= diphist(intenVol(:,:,file),[]);
+    [pks,loc] =findpeaks(hist(hist>100));
+    back = size(pks,2);
+    real = back-1;
+    back = pks(back);
+    real = pks(real);
+    thresh = (bins(find(hist==real,1,'last')) + bins(find(hist==real,1,'last')))/2;
     
+    %create the mask and then mask the image
+    mask = uint16(dip_threshold(intenVol(:,:,file),thresh,0,1,0));
+    intenVol(:,:,file) = double(mask).* intenVol(:,:,file);
+    intenVol_masked = intenVol(:,:,file);
+    intenVol_masked (intenVol_masked == 0)= [];
+    
+     fd = fopen([path_dest  num2str(file-1) '.ct'], 'w+');
+     fwrite(fd, intenVol(:,:,file), fmt);
+     fclose(fd);
     
     
     
