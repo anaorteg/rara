d69a3733e49b2369f13f7f31eceeb85694e75948
nikhilmurty <nikhilsanjaymurty@gmail.com>
2017-07-06 11:46:33 +0200

    minor fixes to resp signal thresholds

--- cardSignal.m
+++ cardSignal.m
@@ -1,4 +1,4 @@
-function [ data_table, path_dest, avg ] = cardSignal(acqPath,  path_dest, resp_data_table, data_table, means,nof)
+function [ data_table, path_dest, average ] = cardSignal(acqPath,  path_dest, resp_data_table, data_table, means,nof)
 % Cardiac signal extracted from the intensities variations from
 % a Region of Interest
 % Sorts out any "beating" images out from a set of ct scans so that the
@@ -14,21 +14,28 @@ id_v = data_table(1,:);
     plot(id_v, means);
     hline = refline([0 60]);
     hline.Color = 'r';
-
-    
+    hline = refline([0 -60]);
+    hline.Color = 'r';
+   
 %% Find heart rate of mouse
     beat = zeros(1,nof);
     var = zeros(1,nof);
     count = 0;
+    avgs = zeros(1,nof);
+    z = 1;
     %find number of frames per beats
-    for n = 1:nof 
-        if resp_data_table(3, n) == 1
+
+   while (z <= nof)
+       num = z;
+       n = z;
+    while resp_data_table(3, n) == 1
             if means(n) > 2 && means(n) < 60
                 %for normal peaks
                 if n>1 && n < nof
-                    if means(n+1)< means(n)&& means(n) > means(n-1) && means(n+1) > -10 
+                    if means(n+1)< means(n)&& means(n) > means(n-1) && means(n+1) > -60 %finds peaks for the first 2 parts, and makessure the next number is not too low of a dip
                        beat(n)= count;
                        count = 0;
+                       count = count + 1;
                        copyfile ([acqPath num2str(n-1) '.ct'],path_dest);
                        fprintf('Saving frozen file %i\n',n)
                        data_table(3,n) = 1;
@@ -36,7 +43,7 @@ id_v = data_table(1,:);
                 end
                 
                 %for peak at n = 1
-                if n==1 && means(n+1)< means(n)
+                if n==1 && means(n+1)< means(n) %finds a peak at the start if there is one
                    beat(n) = 0;
                    data_table(3,n) = 1;
                    copyfile ([acqPath num2str(n-1) '.ct'],path_dest);
@@ -44,27 +51,26 @@ id_v = data_table(1,:);
                 end
                 
                 %for peak at n=nof
-                if n==nof && means(n) > means(n-1)
+                if n==nof && means(n) > means(n-1) %finds a peak if there is one at the end
                    beat(n) = count;
                    data_table(3,n) = 1;
                    copyfile ([acqPath num2str(n-1) '.ct'],path_dest);
                    fprintf('Saving frozen file %i\n',n)                   
                 end
-            elseif means(n) >= 20 || means(n) < -20 || means(n)== 0
-                count = count +0;
+            elseif means(n) >= 60 || means(n) < -60 || means(n)== 0 %makes sure that peaks that are too big arent counted
+                %count = count +0;
                 data_table(3,n) = 0;
             else
                 count = count +1;
                 data_table(3,n) = 0;
             end
-        end %if for resp_data_table
-        
+        n = n+1;
     end %for frames of beat
     
     %add time for changing angle
     counts = 0;
     avg = 0;
-    for num = 1:nof     
+    while resp_data_table(3, num) == 1     
         if beat(num) > 0
            if num > 7
               var(num) = beat(num) - mod(num,8); %used to find the number of times the angle has changed
@@ -74,12 +80,22 @@ id_v = data_table(1,:);
         end
         beat(num) = (beat(num)*0.05) + var(num);
         avg = avg + beat(num);
+        num = num+1;
     end %for rate
-    
- %% Find average heart rate
+  % Find average heart rate
     avg = avg/counts;
     avg = 60/avg;
-%     fprintf('The average heart rate is %.2f beats per minute. Number of beats is %f \n',avg, counts) 
+    avgs(n) = avg;
+    
+    z = n;
+    z = z+1;
+   end % main while loop
+   
+   
+   avgs(isnan(avgs))= [];
+   avgs(avgs == 0)= [];
+   average = mean(avgs);
+%     fprintf('The average heart rate is %.2f beats per minute. \n',average) 
     save([acqPath 'c1\data_table.mat'],'data_table')
     
         
--- respSignal.m
+++ respSignal.m
@@ -1,4 +1,4 @@
-function [data_table , avg] = respSignal(acqPath,path_dest, data_table, means,nof)
+function [data_table , average] = respSignal(acqPath,path_dest, data_table, means,nof)
 % Respiratory signal extracted from the intensities variations from
 % a Region of Interest
 % Sorts out any "breathing" images out from a set of ct scans so that the
@@ -22,14 +22,27 @@ hline.Color = 'r';
     breath = zeros(1,nof);
     var = zeros(1,nof);
     count = 0;
+    avg = zeros(1,nof);
     %find number of frames per breath
     for n = 1:nof
             if n>1 && n < nof 
-                if means(n) > 170 + means(n-1)
+                if means(n) > 100 + means(n-1)
                     if means(n+1)< means(n)&& means(n) > means(n-1) && means(n+1) > -50
                        breath(n)= count;
                        count = 0;
-                        data_table(3,n) = 2;
+                       count = count +1;
+                       data_table(3,n) = 2;
+                       if n>7 % get average breathing rate
+                           var(n) = breath(n) - mod(n,8); %used to find the number of times the angle has changed
+                           var(n)= fix(var(n)/8);
+                           breath(n) = (breath(n)*0.05) + var(n);
+                           avg(n) = breath(n);
+                       else
+                           var(n)= fix(var(n)/8);
+                           breath(n) = (breath(n)*0.05) + var(n);
+                           avg(n) = breath(n);                           
+                       end
+
                     end
                 end
              
@@ -37,18 +50,24 @@ hline.Color = 'r';
             %for peak at n = 1
             elseif n==1 && means(n+1)< means(n) && means(n+1) > 0
                breath(n) = 0;
-                data_table(3,n) = 2;
+               data_table(3,n) = 2;
             
 
             %for peak at n=nof
             elseif n==nof && means(n) > means(n-1)
                breath(n) = count;
-                data_table(3,n) = 2;
+               data_table(3,n) = 2;
+               if n>7 % get average breathing rate
+                   var(n) = breath(n) - mod(n,8); %used to find the number of times the angle has changed
+                   var(n)= fix(var(n)/8);
+                   breath(n) = (breath(n)*0.05) + var(n);
+                   avg(n) = breath(n);
+               end                
             end
             
             %save files that dont have breathing
             if n>1 && n<nof && means(n) < 150
-                    if abs(means(n)-means(n-1))<100 && means(n) - means(n+1)>-100
+                    if abs(means(n)-means(n-1))<50 && means(n) - means(n+1)>-50
                         data_table(3,n) = 1 ;
                         copyfile ([acqPath num2str(n-1) '.ct'],path_dest);
                         fprintf('Saving heartbeat file %i\n',n)
@@ -61,27 +80,14 @@ hline.Color = 'r';
         count = count +1;
     end %for frames of breath
     
-    %add time for changing angle
-    counts = 0;
-    avg = 0;
-    for num = 1:nof     
-        if breath(num) > 0
-           if num > 7
-              var(num) = breath(num) - mod(num,8); %used to find the number of times the angle has changed
-              var(num)= fix(var(num)/8);
-           end
-           counts = counts +1;
-        end
-        breath(num) = (breath(num)*0.05) + var(num);
-        avg = avg + breath(num);
-    end %for rate
-    
+
 %% Find average breathing rate
-    avg = avg/counts;
-    avg = 60/avg;
-%     fprintf('The average breathing rate is %.2f breaths per minute. Number of breaths taken is %d.\n',avg, counts) 
+    avg(avg==0) = [];
+    avg(isnan(avg))= [];
+    average = mean(avg);
+    average = 60/average;
+ 
     
 save([acqPath 'p1\data_table.mat'],'data_table')
         
-
 end %function
